a741b299b1ef4355353f967e65e2f0a3967b9d9f
Naveen-Ayyavoo <naveenranjith2003@gmail.com>
2025-08-02 14:47:03 +0530

    Hard level Beginner Project

---
+++ src/HardEmployee.java
@@ -0,0 +1,44 @@
+import javax.management.StandardEmitterMBean;
+import java.util.Scanner;
+
+public class HardEmployee {
+
+    int numOfEmployees;
+    String name;
+    double salary;
+    String designation;
+
+    HardEmployee(){
+
+            System.out.print("Enter the Name:");
+            name = Hard.scanner.nextLine();
+
+            System.out.print("Enter the Salary: ");
+            salary = Hard.scanner.nextDouble();
+            Hard.scanner.nextLine();
+
+            System.out.print("Enter the Designation: ");
+            designation = Hard.scanner.nextLine();
+
+    }
+
+    void applyBonus(){
+
+        if (designation.toLowerCase().contains("manager"))  salary += 7000;
+        else if (designation.toLowerCase().contains("developer") || designation.toLowerCase().contains("engineer"))  salary += 5000;
+        else if (designation.toLowerCase().contains("associative"))  salary += 3000;
+        else if (designation.toLowerCase().contains("executive"))  salary += 2000;
+        else salary += 1000;
+
+    }
+
+    void printDetails(){
+
+        System.out.println("Name: " + this.name);
+        System.out.println("Salary: " + this.salary);
+        System.out.println("Designation: " + this.designation);
+        System.out.println();
+
+    }
+
+}
